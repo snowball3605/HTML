097ac7fb6d17544cb413fc6e90835ca5c6b124cb
snowball_213 <81451339+weslychan1212@users.noreply.github.com>
2022-06-08 21:46:43 +0800

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>TEST</title>
+    </head>
+    <body>
+
+    <?php 
+        echo("Hello World")
+    ?>
+
+
+    </body>
+</html>
